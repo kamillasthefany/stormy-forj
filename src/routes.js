22c58d46b18b344902971ca1632201dcc6273b77
Kamilla <kamilla.rodrigues@auvo.com.br>
2021-10-16 18:03:16 -0300

    teste youtube (não funciona)

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,27 @@
 const express = require('express');
 
 const routes = express.Router();
-const Usuario = require('./controllers/usuarios.controller');
-const Produto = require('./controllers/produtos.controller');
+// const Usuario = require('./controllers/usuarios.controller');
+// const Produto = require('./controllers/produtos.controller');
 const WebScrapper = require('./controllers/webscrapper.controller');
 
 routes.get('/', function (request, response) {
   response.json('Olá');
 });
 
-//Usuário
-routes.post('/usuario', Usuario.create);
-routes.get('/usuario', Usuario.index);
-routes.get('/usuario.details/:_id', Usuario.details);
-routes.delete('/usuario/:_id', Usuario.delete);
-routes.put('/usuario', Usuario.update);
+// //Usuário
+// routes.post('/usuario', Usuario.create);
+// routes.get('/usuario', Usuario.index);
+// routes.get('/usuario.details/:_id', Usuario.details);
+// routes.delete('/usuario/:_id', Usuario.delete);
+// routes.put('/usuario', Usuario.update);
 
-//Produto
-routes.post('/produto', Produto.create);
-routes.get('/produto', Produto.index);
-routes.get('/produto.details/:_id', Produto.details);
-routes.delete('/produto/:_id', Produto.delete);
-routes.put('/produto', Produto.update);
+// //Produto
+// routes.post('/produto', Produto.create);
+// routes.get('/produto', Produto.index);
+// routes.get('/produto.details/:_id', Produto.details);
+// routes.delete('/produto/:_id', Produto.delete);
+// routes.put('/produto', Produto.update);
 
 //Scrapper
 routes.post('/scrapper', WebScrapper.index);
